b4dc41d257ef4aa49bb6be7648f7c4409efa59da
terminalitis <io@rchung.it>
2015-03-19 19:47:36 -0700

    commit[0]

---
+++ CCNA/Chapter3.py
@@ -0,0 +1,7 @@
+print	"VLSM = Variable Length Subnet Masking\n"\
+	"----\n"\
+	"VLSM is important to modern IP networks because it allows us to identify each subnet by its mask number.\n"\
+	"A routing protocol must be able to *advertise the mask for each subnet in the routing update* to support VLSM.\n"\
+	"\n"\
+	"DEFINITION= The capability to apply more than one subnet mask to a given class of addresses throughout a routed system.\n"\
+	"\n"\
