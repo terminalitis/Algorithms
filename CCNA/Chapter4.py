40abed32786b409a6d6a827e5f5dc77feab1f262
Raymond Chung <io@rchung.it>
2015-03-23 15:38:12 -0700

    commit[11]

---
+++ CCNA/Chapter4.py
@@ -0,0 +1,24 @@
+# Python 3.4.3
+# Algorithms/CCNA/Chapter4.py
+
+print ("Certified Cisco Network Associate\n"\
+            "Chapter 4 - Working with Cisco Equipment\n"\
+            )
+
+print ("CramSaver\n"\
+            "1. Local Device Management\n"\
+            "C: Console\n"\
+            "2. Connect router FastEthernet 0/0 directly to workstation NIC\n"\
+            "E: Crossover\n"
+            )
+
+print ("Console Port Connection\n"\
+            "special Rollover Cable\n"\
+            "1:8, 2:7, 3:6, 4:5, 5:4, 6:3, 7:2, 8:1\n"\
+            "One End = RJ-45 for console port\n"\
+            "Other end = 9-pin serial connector\n"\
+            "Terminal Application: HyperTerminal, Procomm, TeraTerm, SecureCRT\n"\
+            "Baud Rate: 9600\nData Bits: 8\nParity: None\nStop Bits: 1\nFlow Control: None\n"\
+            )
+
+print ("")
